eaacd99a4a05e9c93f626c1e0b83df910616c3dc
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-13 00:17:10 +0100

    Update homepage.py

--- ~/app/homepage.py
+++ ~/app/homepage.py
@@ -4,3 +4,8 @@ import streamlit as st
 
 st.header("Loan Prediction App ")
 st.sidebar.markdown("💳 Home Page")
+
+expander_1 = st.sidebar.expander("PLEASE READ BEFORE YOU BEGIN")
+expander_1.markdown("""<b>This App</b> has been designed out of passion to 
+make <b>business intelligence</b> simple, easy, and efficient for business owners
+with <b>no business analysis experience</b>. """, unsafe_allow_html=True)
