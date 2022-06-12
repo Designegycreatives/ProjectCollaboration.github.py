179768c4bc6da2afee22e1ce3b5ff414556650ec
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-13 00:07:34 +0100

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -7,18 +7,6 @@ st.sidebar.markdown(("💳 Home Page")
 
 
        
-# Image For Page
-file_ = open("image1.jpg", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
-
-st.markdown(
-       f'<img src="data:image/gif;base64,{data_url}" alt="dashboard gif">',
-       unsafe_allow_html=True
-)
-       
-      
-       
+  
        
        
