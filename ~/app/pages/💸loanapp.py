3492c0171af7c7dd91a0262fbadd960b6658f0d6
AnuoluwapoDS <83256563+Designegycreatives@users.noreply.github.com>
2022-06-13 00:53:21 +0100

    Update 💸loanapp.py

--- ~/app/pages/💸loanapp.py
+++ ~/app/pages/💸loanapp.py
@@ -17,15 +17,7 @@ trained_model = pickle.load(open(
 
 
 
-# Image For Page
-file_ = open("image1.jpg", "rb")
-contents = file_.read()
-data_url = base64.b64encode(contents).decode("utf-8")
-file_.close()
 
-st.markdown(
-    f'<img src="data:image/gif;base64,{data_url}" alt="dashboard gif">',
-    unsafe_allow_html=True)
 
 
     
